9c6f81b48d8c66bba614e231c357f7b7080f0853
nhamtung <nhamtung125@gmail.com>
2021-01-23 12:49:14 +0700

    param_runtime

--- param_pkg/README.md
+++ param_pkg/README.md
@@ -11,3 +11,10 @@
 - Build: $catkin_make
 - Source: $source devel/setup.bash
 - Run: $roslaunch param_pkg loadParam.launch
+
+# dynamic_reconfigure
+- Directory to workspace
+- Build: $catkin_make
+- Source: $source devel/setup.bash
+- Run: $roslaunch param_pkg param_runtime.launch
+- Open new sheet and run: $rosrun rqt_reconfigure rqt_reconfigure
---
+++ param_pkg/src/param_runtime.cpp
@@ -0,0 +1,33 @@
+#include <ros/ros.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <param_pkg/robotConfig.h>
+
+// Reference: https://answers.ros.org/question/270034/run-rosparam-load-in-runtime/
+
+void callback(param_pkg::robotConfig &config, uint32_t level) {
+    ROS_INFO("bool_param: %d", config.bool_param);
+    ROS_INFO("str_param: %s", config.str_param.c_str());
+
+    ROS_INFO("L: %f", config.L);
+    ROS_INFO("R: %f", config.R);
+    ROS_INFO("K: %d", config.K);
+    ROS_INFO("SpeedMotorMax: %lf", config.SpeedMotorMax);
+    ROS_INFO("SpeedMotorMin: %d", config.SpeedMotorMin);
+    ROS_INFO("thesholdQualityPose: %d", config.thesholdQualityPose);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "param_runtime");
+    ROS_INFO("param_runtime.cpp - Create node param_runtime");
+
+    dynamic_reconfigure::Server<param_pkg::robotConfig> server;
+    dynamic_reconfigure::Server<param_pkg::robotConfig>::CallbackType f;
+
+    f = boost::bind(&callback, _1, _2);
+    server.setCallback(f);
+
+    ros::spin();
+
+    return 0;
+}
